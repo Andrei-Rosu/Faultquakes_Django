2a9ee0a0ac2b2b56481328ea8abfb9afb563c2c7
Andrei-Rosu <jamaika80@yahoo.com>
2018-10-20 12:10:23 +0200

    First steps

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/administrateur/PycharmProjects/Faultquakes_Django/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
