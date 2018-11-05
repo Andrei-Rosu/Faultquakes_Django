2e27187a66ba0b6699b33286f8355b636b3499ab
Andrei-Rosu <jamaika80@yahoo.com>
2018-11-05 17:17:29 +0100

    make it bettersss

--- Faultquakes_Django/wsgi.py
+++ Faultquakes_Django/wsgi.py
@@ -8,11 +8,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise.django import DjangoWhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Faultquakes_Django.settings')
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
 
+application = get_wsgi_application()
